50a39d8e615e3cbe97b40215d1f8923dddd8fd04
Jackson Ferreira de Andrade Mafra <jacksonfdam@gmail.com>
2015-05-30 15:38:54 -0300

    - Fix das Permissoes  - Teste de Notificação  - icone da Notificacao

--- wear/src/main/java/br/com/ajudafacil/ajudafacilapp/MainActivity.java
+++ wear/src/main/java/br/com/ajudafacil/ajudafacilapp/MainActivity.java
@@ -1,7 +1,10 @@
 package br.com.ajudafacil.ajudafacilapp;
 
 import android.app.Activity;
+import android.app.Notification;
+import android.graphics.BitmapFactory;
 import android.os.Bundle;
+import android.support.v4.app.NotificationCompat;
 import android.support.wearable.view.WatchViewStub;
 import android.widget.TextView;
 
@@ -18,6 +21,12 @@ public class MainActivity extends Activity {
             @Override
             public void onLayoutInflated(WatchViewStub stub) {
                 mTextView = (TextView) stub.findViewById(R.id.text);
+                Notification notification = new Notification.Builder(getApplicationContext())
+                        .setSmallIcon(R.drawable.ic_stat_ic_launcher_v2_transparente)
+                        .setVibrate(new long[]{1000, 1000, 1000, 1000, 1000, 1000})
+                        .setContentTitle("Notificacao")
+                        .setContentText("Mensagem")
+                        .build();
             }
         });
     }
