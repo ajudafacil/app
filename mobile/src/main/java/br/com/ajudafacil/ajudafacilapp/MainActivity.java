c15ce752b068a5a6f3667adc149bccbed23002bb
Jackson Ferreira de Andrade Mafra <jacksonfdam@gmail.com>
2015-05-30 16:41:54 -0300

    - Action Methods  - Perfil  - Info

--- mobile/src/main/java/br/com/ajudafacil/ajudafacilapp/MainActivity.java
+++ mobile/src/main/java/br/com/ajudafacil/ajudafacilapp/MainActivity.java
@@ -1,18 +1,92 @@
 package br.com.ajudafacil.ajudafacilapp;
 
-import android.support.v7.app.ActionBarActivity;
+import android.app.AlertDialog;
+import android.content.Context;
+import android.content.DialogInterface;
+import android.content.SharedPreferences;
 import android.os.Bundle;
+import android.support.v7.app.ActionBarActivity;
+import android.util.Log;
 import android.view.Menu;
 import android.view.MenuItem;
+import android.view.View;
+import android.widget.Button;
+
+import org.json.JSONArray;
 
 
 public class MainActivity extends ActionBarActivity {
+    Button perfil_action;
+    Button info_action;
+    Button emergency_action;
+    final Context context = this;
+    SharedPreferences prefs = this.getSharedPreferences("ajudafacil", 0);
+    SharedPreferences.Editor editor = prefs.edit();
+
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        perfil_action = (Button) findViewById(R.id.perfil_action);
+        info_action = (Button) findViewById(R.id.info_action);
+        emergency_action = (Button) findViewById(R.id.emergency_action);
+
+        perfil_action.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+
+            }
+        });
+
+        info_action.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+
+            }
+        });
+
+        emergency_action.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+
+                AlertDialog.Builder alertDialogBuilder = new AlertDialog.Builder(
+                        context);
+
+                alertDialogBuilder.setTitle(getBaseContext().getString(R.string.app_name));
+
+                alertDialogBuilder
+                        .setMessage(getBaseContext().getString(R.string.emergency_message))
+                        .setCancelable(false)
+                        .setPositiveButton("Sim", new DialogInterface.OnClickListener() {
+                            public void onClick(DialogInterface dialog, int id) {
+                                callEmergency();
+                            }
+                        })
+                        .setNegativeButton("Não", new DialogInterface.OnClickListener() {
+                            public void onClick(DialogInterface dialog, int id) {
+                                dialog.cancel();
+                            }
+                        });
+                AlertDialog alertDialog = alertDialogBuilder.create();
+                alertDialog.show();
+            }
+        });
     }
+
+    public void callEmergency() {
+
+        try {
+            JSONArray jsonArray2 = new JSONArray(prefs.getString("key", "[]"));
+            for (int i = 0; i < jsonArray2.length(); i++) {
+                Log.d("your JSON Array", jsonArray2.getInt(i) + "");
+            }
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
+    }
+
 
 
     @Override
